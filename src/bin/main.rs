cd20f9d9d2add2c9473ca6a2a9d1b5ac7a72699c
Tiago Deliberali <tiago.deliberali@gmail.com>
2020-05-01 01:33:21 -0300

    first working version of key value store

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,8 @@
-use rustdb::KeyValue;
+use rustdb::{KeyValue, RustDB};
 use serde_json::Value;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
 
 const INSERT_DATA: &[u8; 17] = b"POST / HTTP/1.1\r\n";
 const UPDATE_DATA: &[u8; 16] = b"PUT / HTTP/1.1\r\n";
@@ -9,16 +10,21 @@ const DELETE_DATA: &[u8; 19] = b"DELETE / HTTP/1.1\r\n";
 const READ_DATA: &[u8; 16] = b"GET / HTTP/1.1\r\n";
 
 fn main() {
-    println!("Load database at 7887");
+    println!("Loading database...");
+    let mut db = RustDB::open();
+    db.load();
+    let mut db = Arc::new(Mutex::new(db));
     let listener = TcpListener::bind("127.0.0.1:7887").unwrap();
+    println!("Database ready at 7887");
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        let db_clone = Arc::clone(&db);
+        handle_connection(stream, db_clone);
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, db: Arc<Mutex<RustDB>>) {
     let mut buffer = [0; 512];
     let size = stream.read(&mut buffer).unwrap();
 
@@ -32,34 +38,56 @@ fn handle_connection(mut stream: TcpStream) {
             println!("[ERROR] Keyvalue parse error: {}", err);
             return_http(
                 stream,
-                &format!("HTTP/1.1 400 BAD REQUEST\r\n\r\nInvalid json payload\n{}", err),
+                &format!(
+                    "HTTP/1.1 400 BAD REQUEST\r\n\r\nInvalid json payload\n{}",
+                    err
+                ),
             );
             return;
         }
     };
 
     let (response_code, action) = if buffer.starts_with(READ_DATA) {
-        ("200 OK", format!("action :READ\nid: {}", key_value.key))
+        let key = key_value.get_key_as_string();
+
+        let (response_code, action) = match db.lock().unwrap().get_record(key.to_string()) {
+            Ok(key_value) => ("200 OK", format!("action :READ\nid: {}\nvalue: {}", key_value.get_key_as_string(), key_value.get_value_as_string().unwrap())),
+            Err(err) => ("500 INTERNAL SERVER ERROR", format!("action :READ\nid: {}\nerror: {}", key, err)),
+        };
+        (response_code, action)
     } else if buffer.starts_with(INSERT_DATA) {
-        match key_value.value {
-            Some(value) => (
-                "200 OK",
-                format!("action :INSERT\nid: {}\nvalue: {}", key_value.key, value),
-            ),
+        match key_value.get_value_as_string() {
+            Some(value) => {
+                let key = key_value.get_key_as_string();
+
+                let (response_code, action) = match db.lock().unwrap().save_record(key_value) {
+                    Ok(_) => ("200 OK", format!("action :INSERT\nid: {}\nvalue: {}", key, value)),
+                    Err(err) => ("500 INTERNAL SERVER ERROR", format!("action :INSERT\nid: {}\nvalue: {}\nerror: {}", key, value, err)),
+                };
+
+                (response_code, action)
+            },
             None => ("400 BAD REQUEST", String::from("Missing value to INSERT")),
         }
     } else if buffer.starts_with(UPDATE_DATA) {
-        match key_value.value {
+        match key_value.get_value_as_string() {
             Some(value) => (
                 "200 OK",
-                format!("action :UPDATE\nid: {}\nvalue: {}", key_value.key, value),
+                format!(
+                    "action :UPDATE\nid: {}\nvalue: {}",
+                    key_value.get_key_as_string(),
+                    value
+                ),
             ),
             None => ("400 BAD REQUEST", String::from("Missing value to UPDATE")),
         }
     } else if buffer.starts_with(DELETE_DATA) {
-        ("200 OK", format!("action :DELETE\nid: {}", key_value.key))
+        (
+            "200 OK",
+            format!("action :DELETE\nid: {}", key_value.get_key_as_string()),
+        )
     } else {
-        ("200 OK", String::from("unknown comand"))
+        ("400 BAD REQUEST", String::from("unknown comand"))
     };
 
     let response = format!("HTTP/1.1 {}\r\n\r\n{}", response_code, action);
@@ -92,7 +120,7 @@ fn extract_keyvalue(content: &str) -> Result<KeyValue, String> {
     };
 
     match value {
-        Some(v) => Ok(KeyValue::new(key, v)),
+        Some(v) => Ok(KeyValue::new_from_strings(key, v)),
         None => Ok(KeyValue::new_no_value(key)),
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,143 @@
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::io::{prelude::*, ErrorKind::UnexpectedEof, BufReader, Result, SeekFrom};
+use std::path::Path;
+use crc::crc32;
+
+type ByteString = Vec<u8>;
+type ByteStr = [u8];
+
 pub struct KeyValue {
-    pub key: String,
-    pub value: Option<String>,
+    key: ByteString,
+    value: Option<ByteString>,
 }
 
 impl KeyValue {
-    pub fn new(key: String, value: String) -> KeyValue {
+    pub fn new_from_strings(key: String, value: String) -> KeyValue {
         KeyValue {
-            key,
+            key: key.into_bytes(),
+            value: Option::Some(value.into_bytes()),
+        }
+    }
+
+    pub fn new(key: ByteString, value: ByteString) -> KeyValue {
+        KeyValue {
+            key: key,
             value: Option::Some(value),
         }
     }
 
     pub fn new_no_value(key: String) -> KeyValue {
         KeyValue {
-            key,
+            key: key.into_bytes(),
             value: Option::None,
         }
+    }
+
+    pub fn get_key_as_string(&self) -> String {
+        String::from_utf8_lossy(&self.key).into_owned()
+    }
+
+    pub fn get_value_as_string(&self) -> Option<String> {
+        match &self.value {
+            Some(value) => Some(String::from_utf8_lossy(&value).into_owned()),
+            None => None,
+        }
+    }
+}
+
+pub struct RustDB {
+    f: File,
+    index: HashMap<ByteString, u64>,
+}
+
+impl RustDB {
+    pub fn open() -> RustDB {
+        let f = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(Path::new("./current_db"))
+            .unwrap();
+
+        RustDB {
+            f,
+            index: HashMap::new(),
+        }
+    }
+
+    pub fn load(&mut self) -> Result<()> {
+        let mut f = BufReader::new(&self.f);
+
+        loop {
+            let current_position = f.seek(SeekFrom::Current(0))?;
+            match RustDB::load_record(&mut f) {
+                Ok(key_value) => self.index.insert(key_value.key, current_position),
+                Err(err) => {
+                    match err.kind() {
+                        UnexpectedEof => {
+                            break;
+                        },
+                        _ => return Err(err),
+                    }
+                }
+            };
+        }
+
+        Ok(())
+    }
+
+    fn load_record(file: &mut BufReader<&File>) -> Result<KeyValue> {
+        let checksum = file.read_u32::<LittleEndian>()?;
+        let key_size: usize = file.read_u32::<LittleEndian>()? as usize;
+        let value_size: usize = file.read_u32::<LittleEndian>()? as usize;
+        let total_size: usize = key_size + value_size;
+
+        let mut data = ByteString::with_capacity(total_size);
+
+        {
+            file.by_ref()
+                .take(total_size as u64)
+                .read_to_end(&mut data)?;
+        }
+
+        if checksum != crc32::checksum_ieee(&data) {
+            panic!("invalid checksum");
+        }
+
+        let (key, value) = data.split_at(key_size);
+
+        Ok(KeyValue::new(key.to_vec(), value.to_vec()))
+    }
+
+    pub fn get_record(&self, key: String) -> Result<KeyValue> {
+        let key: Vec<u8> = Vec::from(key);
+        let key_position = *self.index.get(&key).unwrap();
+
+        let mut f = BufReader::new(&self.f);
+        let _ = f.seek(SeekFrom::Start(key_position))?;
+
+        Ok(RustDB::load_record(&mut f).unwrap())
+    }
+
+    pub fn save_record(&mut self, mut key_value: KeyValue) -> Result<usize> {
+        let mut value = key_value.value.unwrap();
+        let key_size = key_value.key.len() as u32;
+        let value_size = value.len() as u32;
+        let total_size = key_size + value_size;
+        let mut data: Vec<u8> = Vec::with_capacity(total_size as usize);
+
+        data.append(&mut key_value.key);
+        data.append(&mut value);
+        let checksum = crc32::checksum_ieee(&data);
+
+        self.f.write_u32::<LittleEndian>(checksum)?;
+        self.f.write_u32::<LittleEndian>(key_size)?;
+        self.f.write_u32::<LittleEndian>(value_size)?;
+        self.f.write(&data)?;
+
+        Ok(self.f.seek(SeekFrom::Current(0))? as usize)
     }
 }
